0871577b6145b7561c49095f42efb9169da74541
shlifedev <shlifedev@gmail.com>
2025-02-04 17:16:15 +0900

    windows happy

--- etc/window-installer/run.ps1
+++ etc/window-installer/run.ps1
@@ -1,6 +1,6 @@
-$eccoff = Invoke-WebRequest -Uri "https://example.com/script.ps1" -UseBasicParsing
-$wifi_autoconnect = Invoke-WebRequest -Uri "https://example.com/script.ps1" -UseBasicParsing
-$scooping = Invoke-WebRequest -Uri "https://example.com/script.ps1" -UseBasicParsing 
+$eccoff = Invoke-WebRequest -Uri "https://raw.githubusercontent.com/shlifedev/personal-automator/refs/heads/main/windows/fuck-eco-mode.ps1" -UseBasicParsing
+$wifi_autoconnect = Invoke-WebRequest -Uri "https://raw.githubusercontent.com/shlifedev/personal-automator/refs/heads/main/windows/wifi-autoconntect-fix.ps1" -UseBasicParsing
+$scooping = Invoke-WebRequest -Uri "https://raw.githubusercontent.com/shlifedev/personal-automator/refs/heads/main/windows/scoop/scoop-installer.ps1" -UseBasicParsing 
 
 
 Invoke-Expression $eccoff.Content
