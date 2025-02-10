c5bf885f4d1aa80e2229950c39695d7f13fc5724
whoiam <shlifedev@gmail.com>
2025-02-10 17:42:51 +0900

    Update run.ps1

--- etc/window-installer/run.ps1
+++ etc/window-installer/run.ps1
@@ -1,8 +1,8 @@
 $eccoff = Invoke-WebRequest -Uri "https://raw.githubusercontent.com/shlifedev/personal-automator/refs/heads/main/windows/fuck-eco-mode.ps1" -UseBasicParsing
 $wifi_autoconnect = Invoke-WebRequest -Uri "https://raw.githubusercontent.com/shlifedev/personal-automator/refs/heads/main/windows/wifi-autoconntect-fix.ps1" -UseBasicParsing
-$scooping = Invoke-WebRequest -Uri "https://raw.githubusercontent.com/shlifedev/personal-automator/refs/heads/main/windows/scoop/scoop-installer.ps1" -UseBasicParsing 
+#$scooping = Invoke-WebRequest -Uri "https://raw.githubusercontent.com/shlifedev/personal-automator/refs/heads/main/windows/scoop/scoop-installer.ps1" -UseBasicParsing 
 
 
 Invoke-Expression $eccoff.Content
 Invoke-Expression $wifi_autoconnect.Content
-Invoke-Expression $scooping.Content
+#Invoke-Expression $scooping.Content
